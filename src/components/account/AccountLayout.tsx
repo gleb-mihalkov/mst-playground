2f5d176b750ee1ec28d626cda64793655ddc0f0a
Gleb Mihalkov <gleb.mihalkov@devim.team>
2021-06-17 16:24:46 +0300

    Проверка доступа в приватную часть

--- src/components/account/AccountLayout.tsx
+++ src/components/account/AccountLayout.tsx
@@ -1,9 +1,23 @@
+import { observer } from 'mobx-react';
 import { FC } from 'react';
+
+import useStore from 'hooks/useStore';
+import Replace from 'components/router/Replace';
+import Route from 'consts/Route';
 
 /**
  * Отображает обрамление страницы в приватной части сайта (шапка, подвал,
- * контейнер содержимого).
+ * контейнер содержимого). Также проверяет, может ли текущий пользователь
+ * находится в приватном разделе.
  */
-const AccountLayout: FC = ({ children }) => <>{children}</>;
+const AccountLayout: FC = ({ children }) => {
+  const store = useStore();
 
-export default AccountLayout;
+  if (!store.auth.isAuthorized) {
+    return <Replace to={Route.LOGIN} />;
+  }
+
+  return <>{children}</>;
+};
+
+export default observer(AccountLayout);
